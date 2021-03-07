fc49e5ec62764e87c8783aa36c5295200b956406
Manuel <mmanuelgg@uma.es>
2021-03-07 17:44:43 +0100

    ArrayList bonico generado

--- Proyecto/src/Main.java
+++ Proyecto/src/Main.java
@@ -11,6 +11,8 @@ public class Main {
         System.out.println("Ivan.");
         System.out.println("Manu");
 
+        ArrayList<String> manu = new ArrayList<>();
+
         System.out.println("\nFinal del programa.");
     }
 }
